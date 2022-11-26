4a3e2fba8f3a2a77998ce1ea00c1a948fa3d58ba
Richard Gigantone <rchrdgigantone@gmail.com>
2022-11-27 01:24:20 +0800

    Installing laravel pakage with jetstream and livewire then added User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create initial user
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin12345'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
